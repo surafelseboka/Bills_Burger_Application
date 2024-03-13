f7cdf4a5e9a2f319473c174be07a9c15079cfa66
surafelseboka <surafelmars20@gmail.com>
2024-03-13 16:39:37 -0700

    Create functionality to Drink.java

--- Drink.java
+++ Drink.java
@@ -1,9 +1,14 @@
 package billsBurgerApplication;
 
-public class Drink extends Item{
+public class Drink{
+    private String drink_type;
+    private String drink_size;
+    private double drink_price;
 
-    public Drink(String name,String description, double price ){
-        super(name,description,price);
+    public Drink(String drink_type, String drink_size){
+        this.drink_type = drink_type;
+        this.drink_size = drink_size;
     }
+
 
 }
